97d6424ce6d5362d6e7148bfb6ca71baa8146606
AIexeus <a.kalenof@yandex.ru>
2025-02-25 10:19:35 +0300

    it's_now_work

--- calculator/src/main/java/ru/krista/example/App.java
+++ calculator/src/main/java/ru/krista/example/App.java
@@ -2,14 +2,52 @@ package ru.krista.example;
 import java.util.Scanner;
 
 public final class App {
-    private App() {
-    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
         String symb = in.next();
         int y = in.nextInt();
-        System.out.println(String.format("X=%d %1.1s Y=%d = ", x, symb, y));
+        switch (symb) {
+            case ("+"):
+            System.out.println(calc.summ(x,y)); 
+            break;
+
+            case ("-"):
+            System.out.println(calc.dif(x,y)); 
+            break;
+
+            case ("*"):
+            System.out.println(calc.mult(x,y)); 
+            break;
+
+            case ("/"):
+            System.out.println(calc.div(x,y)); 
+            break;
+
+            default:
+            System.out.println("неизвестный знак операции");
+        }
+    }
+}
+
+public class calc {
+    public static int summ (int num1, int num2){
+        return num1+num2;
+    }
+    public static int dif (int num1, int num2){
+        return num1-num2;
+    }
+    public static int mult (int num1, int num2){
+        return num1*num2;
+    }
+    public static int div (int num1, int num2){
+        if (num2==0) {
+            System.out.println("нельзя так делать");
+            return 0;
+        } 
+        else {
+            return num1/num2;
+        }
     }
 }
